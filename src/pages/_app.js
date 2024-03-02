e1d9c70de4d3eb20027bc42d7722a9ecd54772d5
ImadMehmood535 <imadmehmood816@gmail.com>
2024-03-02 15:45:43 +0500

    form

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
 import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextUIProvider>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,228 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useForm, Controller } from "react-hook-form";
+import { Button, Input, Radio, RadioGroup, Textarea } from "@nextui-org/react";
+import { toast } from "react-toastify";
 
 export default function Home() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    try {
+      toast(" Email sent successfully", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } catch (error) {
+      toast.error(" Something went wrong, try again", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex justify-center h-full py-20 sm:p-40 bg-gradient-to-r from-blue-400 to-purple-500 items-center">
+      <div className="sm:max-w-[700px] w-full px-4 sm:px-0 flex flex-col items-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white p-8 rounded-[40px]   flex flex-col gap-12 myShadow"
+        >
+          <h1 className="text-3xl font-bold uppercase text-center text-gray-800">
+            Feedback Form
+          </h1>
+          <FeedbackRadio
+            name="satisfactionRating"
+            label="1- On a scale of 1-5, how satisfied are you with the communication and responsiveness of our team?"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackRadio
+            name="effectiveness"
+            label="2- How would you rate the overall effectiveness of the social media campaigns we executed this month?"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackRadio
+            name="satisfied"
+            label="3-  How satisfied are you with the quality and relevance of the content we produced for your social media channels"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackRadio
+            name="engagement"
+            label="4- How do you feel about the level of engagement and interaction from your target audience on social media this month?"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackRadio
+            name="performance"
+            label="5-  How satisfied are you with the analytics and performance reports provided to you?"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackRadio
+            name="efficiency"
+            label="6-  How would you rate the efficiency and effectiveness of our ad spend in driving results for your social media goals?"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackRadio
+            name="strategy"
+            label="7-  On a scale of 1-5, how well do you think our strategy aligns with your overall business goals?"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackRadio
+            name="serviceSupport"
+            label="8-  How satisfied are you with the level of customer service and support provided by our team?"
+            control={control}
+            rules={{ required: true }}
+            errors={errors}
+          />
+          <FeedbackInput
+            name="specificContent"
+            label="9- Are there any specific types of content you would like to see more or less of in the future?"
+            control={control}
+            rules={{ required: false }}
+            errors={errors}
+          />
+          <FeedbackInput
+            name="additionalData"
+            label="10- Is there any additional data or specific insights you would like to see in our reports?"
+            control={control}
+            rules={{ required: false }}
+            errors={errors}
+          />
+          <FeedbackInput
+            name="competitors"
+            label="11- Are there any specific competitors or metrics you would like us to focus on more"
+            control={control}
+            rules={{ required: false }}
+            errors={errors}
+          />
+          <FeedbackInput
+            name="anything"
+            label="12-  Is there anything we can do to improve our support and service?"
+            control={control}
+            rules={{ required: false }}
+            errors={errors}
+          />
+          <div className="w-full">
+            <Controller
+              name="message"
+              control={control}
+              defaultValue=""
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Textarea
+                  {...field}
+                  fullWidth={true}
+                  minRows={6}
+                  label="Are there any areas you think we should focus on to enhance our performance in the coming months?"
+                  labelPlacement="outside"
+                  placeholder="Enter your Suggestions"
+                  classNames={{
+                    label: "text-md",
+                  }}
+                />
+              )}
             />
-          </a>
-        </div>
+            {errors.message && (
+              <p className="text-red-600 py-3">This field is required.</p>
+            )}
+          </div>
+
+          <Button
+            type="submit"
+            className="bg-purple-500 text-white hover:bg-blue-600"
+          >
+            Submit
+          </Button>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+const FeedbackRadio = ({ name, label, control, rules, errors }) => (
+  <div className="w-full">
+    <Controller
+      name={name}
+      control={control}
+      defaultValue=""
+      rules={rules}
+      render={({ field }) => (
+        <RadioGroup
+          {...field}
+          classNames={{
+            label: "text-black",
+          }}
+          orientation="horizontal"
+          label={label}
+          color="danger"
+        >
+          {[1, 2, 3, 4, 5].map((value) => (
+            <Radio
+              key={value}
+              value={String(value)}
+              description={String(value)}
+            />
+          ))}
+        </RadioGroup>
+      )}
+    />
+    {errors[name] && (
+      <p className="text-red-600 py-3">This field is required.</p>
+    )}
+  </div>
+);
+
+const FeedbackInput = ({ name, label, control, rules, errors }) => (
+  <div className="w-full pt-3">
+    <Controller
+      name={name}
+      control={control}
+      defaultValue=""
+      rules={rules}
+      render={({ field }) => (
+        <Input
+          {...field}
+          type="text"
+          label={label}
+          labelPlacement="outside"
+          placeholder="Enter your answer here"
+          className="text-black"
+          classNames={{
+            label: "text-md",
+          }}
+        />
+      )}
+    />
+    {errors[name] && (
+      <p className="text-red-600 py-3">This field is required.</p>
+    )}
+  </div>
+);
